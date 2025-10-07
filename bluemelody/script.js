0af8cc07d20a5bfda4f63a0be69422277b8f0dad
AEDTP WORLD <label79736@label-engine.com>
2025-10-07 02:39:40 +0000

    Update script.js

--- bluemelody/script.js
+++ bluemelody/script.js
@@ -1,1 +1,27 @@
-404: Not Found
+let darkTheme = false;
+
+function toggleTheme() {
+  if (!darkTheme) {
+    document.documentElement.style.setProperty('--btn-bg', 'rgb(15, 37, 57)');
+    document.documentElement.style.setProperty('--bg-color', '#222');
+    document.documentElement.style.setProperty('--text-color', 'rgb(231, 230, 230)');
+    darkTheme = true;
+  } else {
+    document.documentElement.style.setProperty('--btn-bg', 'rgb(30, 144, 255)');
+    document.documentElement.style.setProperty('--bg-color', 'white');
+    document.documentElement.style.setProperty('--text-color', 'rgb(50, 50, 50)');
+    darkTheme = false;
+  }
+}
+
+function changeFont(font) {
+  document.documentElement.style.setProperty('--font-family', font);
+}
+
+function changeFontSize(size) {
+  document.documentElement.style.setProperty('--font-size', size + 'px');
+}
+
+function changeButtonColor(color) {
+  document.documentElement.style.setProperty('--btn-bg', color);
+}
